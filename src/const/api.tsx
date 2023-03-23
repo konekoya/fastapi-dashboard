d6342d1801337c8a9ad667ce2a69386ef068c01a
Joshua <konekoya.dev@gmail.com>
2023-03-23 15:14:52 +0800

    Wire up login api (#1)

---
+++ src/const/api.tsx
@@ -0,0 +1,3 @@
+const port = 8000;
+const url = 'http://localhost';
+export const BASE_URL = `${url}:${port}`;
---
+++ src/pages/signup.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const SignUp: NextPage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen mx-auto">
+      This is the sign up page
+    </div>
+  );
+};
+
+export default SignUp;
